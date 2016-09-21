de98003f00201497c349b6fc715b8ee9b5d61f75
jimmy <kji0205@gmail.com>
2016-09-21 13:38:00 +0900

    3

---
+++ client/templates/application/layout.js
@@ -0,0 +1,100 @@
+
+Template.layout.onCreated(function () {
+    fixed = false;
+});
+
+Template.layout.onRendered(function(){
+    $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    onMapClickHandler= function(e) {
+        var that = $(this);
+        // Disable the click handler until the user leaves the map area
+        that.off('click', onMapClickHandler);
+        // Enable scrolling zoom
+        that.find('iframe').css("pointer-events", "auto");
+        // Handle the mouse leave event
+        that.on('mouseleave', onMapMouseleaveHandler);
+    }
+
+    onMapMouseleaveHandler= function(e) {
+        var that = $(this);
+        that.on('click', onMapClickHandler);
+        that.off('mouseleave', onMapMouseleaveHandler);
+        that.find('iframe').css("pointer-events", "none");
+    }
+
+});
+
+Template.layout.helpers({
+    counter() {
+        return Template.instance().counter.get();
+    },
+    onMapMouseleaveHandler(e) {
+        var that = $(this);
+        that.on('click', onMapClickHandler);
+        that.off('mouseleave', onMapMouseleaveHandler);
+        that.find('iframe').css("pointer-events", "none");
+    },
+    onMapClickHandler(e) {
+        var that = $(this);
+        // Disable the click handler until the user leaves the map area
+        that.off('click', onMapClickHandler);
+        // Enable scrolling zoom
+        that.find('iframe').css("pointer-events", "auto");
+        // Handle the mouse leave event
+        that.on('mouseleave', onMapMouseleaveHandler);
+    }
+});
+
+Template.layout.events({
+    'click #menu-close'(e){
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    },
+    'click #menu-toggle'(e){
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    },
+    'scroll'(e){
+        e.preventDefault();
+        if ($(document).scrollTop() > 250) {
+            if (!fixed) {
+                fixed = true;
+                // $('#to-top').css({position:'fixed', display:'block'});
+                $('#to-top').show("slow", function() {
+                    $('#to-top').css({
+                        position: 'fixed',
+                        display: 'block'
+                    });
+                });
+            }
+        } else {
+            if (fixed) {
+                fixed = false;
+                $('#to-top').hide("slow", function() {
+                    $('#to-top').css({
+                        display: 'none'
+                    });
+                });
+            }
+        }
+    },
+    'click .map'(e) {
+        e.preventDefault();
+        onMapClickHandler(e);
+    },
+});
+
+
+
