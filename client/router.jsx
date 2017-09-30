6cc6a297a1fac3757cc2c6ba3da6db7410bb09d1
jimmy <kji0205@gmail.com>
2017-09-30 09:14:42 +0900

    remove iron:router

--- client/router.jsx
+++ client/router.jsx
@@ -11,6 +11,19 @@ FlowRouter.route('/todo', {
         // mount(null, {
         //     content: (<App />)
         // })
-        render(<App />, document.getElementById('render-target'));
+        render(<App />, document.getElementById("render-target"));
     }
 });
+
+FlowRouter.route('/mine', {
+  action () {
+    BlazeLayout.render('mine', {main: 'mine', data: ''})
+  }
+});
+
+// FlowRouter.route('/posts/:_id', {
+//   action (params) {
+//     var post = Posts.findOne({_id: params._id});
+//     BlazeLayout.render('layout', {main: 'postContent', data: post})
+//   }
+// });
