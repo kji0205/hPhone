56e361c458c8a34844aa5cee220f8fa72d3a29bf
jimmy <kji0205@gmail.com>
2017-09-30 09:51:21 +0900

    Refactor route code base

--- client/router.jsx
+++ client/router.jsx
@@ -5,21 +5,21 @@ import {mount} from 'react-mounter';
 import {MainLayout} from './layouts/MainLayout.jsx';
 import App from '../imports/ui/App.jsx';
 
-FlowRouter.route('/todo', {
-    action() {
-        // <App />
-        // mount(null, {
-        //     content: (<App />)
-        // })
-        render(<App />, document.getElementById("render-target"));
-    }
-});
+// FlowRouter.route('/todo', {
+//     action() {
+//         // <App />
+//         // mount(null, {
+//         //     content: (<App />)
+//         // })
+//         render(<App />, document.getElementById("render-target"));
+//     }
+// });
 
-FlowRouter.route('/mine', {
-  action () {
-    BlazeLayout.render('mine', {main: 'mine', data: ''})
-  }
-});
+// FlowRouter.route('/mine', {
+//   action () {
+//     BlazeLayout.render('mine', {main: 'mine', data: ''})
+//   }
+// });
 
 // FlowRouter.route('/posts/:_id', {
 //   action (params) {
@@ -27,3 +27,27 @@ FlowRouter.route('/mine', {
 //     BlazeLayout.render('layout', {main: 'postContent', data: post})
 //   }
 // });
+
+var ionRoutes = FlowRouter.group({
+  prefix: '',   // 임시 prefix: '/ion',
+  name: '',     // ion
+  triggersEnter: [function(context, redirect) {
+    console.log('running group triggers');
+  }]
+});
+
+// handling /admin route
+ionRoutes.route('/todo', {
+  action () {
+    render(<App />, document.getElementById("render-target"));
+  },
+  triggersEnter: [function(context, redirect) {
+    console.log('running /ion trigger');
+  }]
+});
+
+ionRoutes.route('/mine', {
+  action: function() {
+    BlazeLayout.render('mine', null);
+  }
+});
